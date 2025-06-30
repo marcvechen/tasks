030cb0ab6b5a1f198dd5e97a8b3e714eb56e386c
Danil <taz98636@gmail.com>
2025-06-30 18:38:57 +0300

    first commit

---
+++ 06-data-types/finish.js
@@ -0,0 +1,31 @@
+/** ЗАДАЧА 6 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+const str = "str",
+  num = 5,
+  boolean1 = false,
+  null1 = null,
+  undefined1 = undefined,
+  obj = {
+    num: 4,
+  },
+  array = [5, 1];
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof boolean1);
+console.log(typeof null1);
+console.log(typeof undefined1);
+console.log(typeof obj);
+console.log(typeof array);
---
+++ 12-create-object/finish.js
@@ -0,0 +1,18 @@
+/** ЗАДАЧА 12 - Создание объекта
+ *
+ * 1. Создайте объект с тремя свойствами:
+ *  - name
+ *  - surname
+ *  - favoriteNumber
+ *
+ * 2. Выведите в консоль строку
+ * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
+ */
+const obj = {
+  name: "Danil",
+  surname: "Goloschapov",
+  favoriteNumber: 5,
+};
+console.log(
+  `My name is ${obj.name} ${obj.surname} and my favorite number is ${obj.favoriteNumber}`
+);
