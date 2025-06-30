5ee4757a8b31c2bb957f189f702807f1278d6ac6
Danil <taz98636@gmail.com>
2025-06-30 20:17:46 +0300

    4-th com

--- 34-array-of-objects/finish.js
+++ 34-array-of-objects/finish.js
@@ -11,3 +11,26 @@
  *
  * 4. Выведите результирующий массив в консоль
  */
+const myArray = [
+  (car = {
+    carBrand: "audi",
+    price: "1000",
+    isAvailableForSale: true,
+  }),
+  (car = {
+    carBrand: "bmw",
+    price: "1000",
+    isAvailableForSale: false,
+  }),
+  (car = {
+    carBrand: "benz",
+    price: "1000",
+    isAvailableForSale: true,
+  }),
+];
+myArray[3] = car = {
+  carBrand: "fiat",
+  price: "1000",
+  isAvailableForSale: false,
+};
+console.log(myArray);
