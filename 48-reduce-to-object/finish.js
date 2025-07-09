8ab4b7fee4aaaa2af9d46794c6c8c5e229c2701d
Danil <taz98636@gmail.com>
2025-07-09 17:05:46 +0300

    commit

--- 48-reduce-to-object/finish.js
+++ 48-reduce-to-object/finish.js
@@ -9,32 +9,43 @@
 
 const inputProducts = [
   {
-    title: 'Phone case',
+    title: "Phone case",
     price: 23,
     quantity: 2,
-    category: 'Accessories',
+    category: "Accessories",
   },
   {
-    title: 'Android phone',
+    title: "Android phone",
     price: 150,
     quantity: 1,
-    category: 'Phones',
+    category: "Phones",
   },
   {
-    title: 'Headphones',
+    title: "Headphones",
     price: 78,
     quantity: 1,
-    category: 'Accessories',
+    category: "Accessories",
   },
   {
-    title: 'Sport Watch',
+    title: "Sport Watch",
     price: 55,
     quantity: 2,
-    category: 'Watches',
+    category: "Watches",
   },
-]
-
-console.log(quantitiesByCategories(inputProducts))
+];
+function quantitiesByCategories(products) {
+  const counter = products.reduce((acc, product) => {
+    const category = product.category;
+    if (acc[category]) {
+      acc[category] += product.quantity;
+    } else {
+      acc[category] = product.quantity;
+    }
+    return acc;
+  }, {});
+  return counter;
+}
+console.log(quantitiesByCategories(inputProducts));
 /* {
   Accessories: 3,
   Phones: 1,
--- 50-template-literals/finish.js
+++ 50-template-literals/finish.js
@@ -8,17 +8,26 @@
  */
 
 // ТЕСТ 1
-const myNumber = 9
-console.log(templateLiteral(myNumber))
+
+const myNumber = 9;
+console.log(templateLiteral(myNumber));
 /* 
 Число 9.
 Это число меньше 10.
 Квадратный корень этого числа - 3.
 */
-
+function templateLiteral(num) {
+  const sqrt = Math.sqrt(num);
+  const minusNum = num + 1;
+  if (num >= 10) {
+    return `Число ${num}. \nЭто число больше или равно 10. \nКвадратный корень этого числа - ${sqrt} `;
+  } else {
+    return `Число ${num}. \nЭто число меньше ${minusNum}. \nКвадратный корень этого числа - ${sqrt} `;
+  }
+}
 // ТЕСТ 2
-const myAnotherNumber = 25
-console.log(templateLiteral(myAnotherNumber))
+const myAnotherNumber = 25;
+console.log(templateLiteral(myAnotherNumber));
 /* 
 Число 25.
 Это число больше или равно 10.
